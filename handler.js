0b424074a615540ad17badb881df21c77487873a
Gabriel Chen <gabriel.luke.chen@gmail.com>
2021-11-28 01:46:55 -0800

    patch

--- handler.js
+++ handler.js
@@ -38,8 +38,102 @@ module.exports.postConfirmationRegisterUserID = (event, context, callback) => {
   });
 };
 
-module.exports.updateUserInformation = (event, context, callback) => {
+function getCallerAccessLevel(userId, callback) {
+  const params = {
+    TableName: process.env.DYNAMODB_TABLE,
+    Item: {
+      'UserID': {S: userId}
+    }
+  }
 
+  return ddb.getItem(params, callback);
+}
+
+module.exports.updateUserInformation = (event, context, callback) => {
+  const { userId } = event.pathParameters;
+  const { Street, City, State, Zipcode, AccessLevel } = JSON.parse(event.body);
+  const userName = event.requestContext.authorizer.jwt.claims.userName;
+  console.log(`updateUserInformation: authorizer claim, userName ${userName}`);
+  var requesterAccessLevel;
+
+  if (userId != userName) {
+    requesterAccessLevel = await getCallerAccessLevel(userName)
+      .then(data => data.AccessLevel)
+      .error(err => {
+        console.error("updateUserInformation: failed to get caller access level error:", err);
+        callback(null, {statusCode: 403});
+      });
+
+    if (requesterAccessLevel != "Admin"){
+      console.info(`updateUserInformation: insufficient permissions to access resource, requester: ${userName}, userId ${userId}, al ${data.AccessLevel}`);
+      callback(null, {statusCode: 403});
+    }
+    console.log("updateUserInformation: jwt claims verified successfully");
+  }
+
+  const params = {
+    TableName: process.env.DYNAMODB_TABLE,
+    Key: {
+      'UserID': userId,
+    },
+    UpdateExpression: "set Address:Street=:street, Address.City=:city, Address.State=:state,Address.Zipcode=:zipcode",
+    ExpressionAttributeValues:{
+      ":street":Street,
+      ":city":City,
+      ":state":State,
+      ":zipcode":Zipcode,
+    },
+    ReturnValues:"UPDATED_NEW"
+  };
+
+  ddb.update(params, function(err, data) {
+    if (err) {
+      console.error("updateUserInformation: failed to update address information, error", error);
+      callback(null, { statusCode:500 });
+    } else {
+      console.log("updateUserInformation: successfully updated addresses, data", data);
+    }
+  });
+
+  if (requesterAccessLevel != "Admin") {
+    callback(null, {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true,
+      },
+      message: 'successfully updated address',
+    });
+  }
+
+  const updateAccessLevel = {
+    TableName: process.env.DYNAMODB_TABLE,
+    Key: {
+      'UserID': userId,
+    },
+    UpdateExpression: "set AccessLevel=:a",
+    ExpressionAttributeValues:{
+      ":a":AccessLevel,
+    },
+    ReturnValues:"UPDATED_NEW"
+  };
+
+  ddb.update(params, function(err, data) {
+    if (err) {
+      console.error("updateUserInformation: failed to update accesslevel, error", error);
+      callback(null, { statusCode:500 });
+    } else {
+      console.log("updateUserInformation: successfully updated accesslevel, data", data);
+      callback(null, {
+        statusCode:200,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Credentials': true,
+        },
+        message: 'successfully AccessLevel',
+      });
+    }
+  });
 }
 
 
